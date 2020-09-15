48ac884c134bc23ef175764009ac50e15457334f
github username <theoluyi@gmail.com>
2020-09-15 14:53:31 -0400

    aligns welcome page heading with others, adds border around gif

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,12 +4,12 @@ const Welcome = (props) => {
 
   return (
     <div>
-      <br/>
       <h1 id="header">{props.username? `Welcome to Mindscape, ${props.username}` : "Welcome to Mindscape"}</h1>
-      <img 
-        src="https://media.giphy.com/media/5xtDarsUgCwZMXqcVQA/giphy.gif" 
-        alt="landscape" 
-      />
+      <br/>
+        <img id='landscape' 
+          src="https://media.giphy.com/media/5xtDarsUgCwZMXqcVQA/giphy.gif" 
+          alt="landscape"
+        />
     </div>
   );
 }
