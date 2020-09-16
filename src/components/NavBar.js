ac34707861bacb875a5f8f9a03f045381d139f9a
github username <theoluyi@gmail.com>
2020-09-16 15:43:51 -0400

    conditional rendering of NavBar to reduce screen clutter

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,42 +1,64 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom'
 
-const NavBar = (props) => {
+class NavBar extends React.Component {
 
-  const logout = () => {
+  logout = () => {
     console.log("logout button pressed")
     localStorage.clear()
-    props.clearAppState()
-
-    // this step still requires a refresh but for now that's good enough
-    // this feature is more for easy testing not for UI
+    this.props.clearAppState()
   }
 
-  return(
-    <ul className="nav">
-      <li>
-        <NavLink to="/">Welcome</NavLink>
-      </li>
-      <li>
-        <NavLink to="/meditate">Meditate</NavLink>
-      </li>
-      <li>
-        <NavLink to="/sessions">Sessions</NavLink>
-      </li>
-      <li>
-        <NavLink to="/about">About</NavLink>
-      </li>
-      <li>
-        <NavLink to="/login">Login</NavLink>
-      </li>
-      <li>
-        <NavLink to="/register">Register</NavLink>
-      </li>
-      <li>
-        <NavLink to="/" onClick={logout} >Logout</NavLink>
-      </li>
-    </ul>
-  )
+  render() {
+    // console.log(localStorage.user)
+    const smallNavBar = (
+      localStorage.token? 
+      // if user IS logged in, show logout, omit login and register
+      (<ul className="nav">
+        <li>
+          <NavLink to="/">Welcome</NavLink>
+        </li>
+        <li>
+          <NavLink to="/meditate">Meditate</NavLink>
+        </li>
+        <li>
+          <NavLink to="/sessions">Sessions</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+        <li>
+          <NavLink to="/" onClick={this.logout} >Logout</NavLink>
+        </li>
+      </ul>)
+      :
+      // if user is not logged in, show login and register
+      (<ul className="nav">
+        <li>
+          <NavLink to="/">Welcome</NavLink>
+        </li>
+        <li>
+          <NavLink to="/meditate">Meditate</NavLink>
+        </li>
+        <li>
+          <NavLink to="/sessions">Sessions</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login">Login</NavLink>
+        </li>
+        <li>
+          <NavLink to="/register">Register</NavLink>
+        </li>
+      </ul>)
+    )
+
+    return (smallNavBar)
+  }
 };
+
+
 
 export default NavBar;
